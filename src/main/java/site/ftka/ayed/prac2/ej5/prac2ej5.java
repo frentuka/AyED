d75d5160e6d05f825db21e123fe65b06230ab017
frentuka <frentuka@gmail.com>
2025-04-05 20:15:18 -0300

    ...

---
+++ src/main/java/site/ftka/ayed/prac2/ej5/prac2ej5.java
@@ -0,0 +1,19 @@
+package site.ftka.ayed.prac2.ej5;
+
+/*
+
+    Implemente una clase Java llamada ProfundidadDeArbolBinario
+        que tiene como variable de instancia un árbol binario de números enteros
+        y un método de instancia sumaElementosProfundidad (int p):int
+        el cuál devuelve la suma de todos los nodos del árbol que se encuentren
+        a la profundidad pasada como argumento.
+
+ */
+
+public class prac2ej5 {
+
+    public static void main(String[] args) {
+
+    }
+
+}
---
+++ src/main/java/site/ftka/ayed/prac2/ej6/Transformacion.java
@@ -0,0 +1,41 @@
+package site.ftka.ayed.prac2.ej6;
+
+import site.ftka.ayed.prac2.BinaryTree;
+
+class Transformacion {
+
+    BinaryTree<Integer> tree;
+
+    public Transformacion(BinaryTree<Integer> tree) {
+        this.tree = tree;
+    }
+
+    public BinaryTree<Integer> suma() {
+        int res = 0;
+
+        if (tree.hasLeftChild()) {
+            res+= tree.getLeftChild().getData();
+            tree.addLeftChild(new Transformacion(tree.getLeftChild()).suma());
+            res+= tree.getLeftChild().getData();
+        }
+
+        if (tree.hasRightChild()) {
+            res+= tree.getRightChild().getData();
+            tree.addRightChild(new Transformacion(tree.getRightChild()).suma());
+            res+= tree.getRightChild().getData();
+        }
+
+        if (tree.isLeaf())
+            tree.setData(0);
+        else
+            tree.setData(res);
+
+        return tree;
+    }
+
+
+
+
+
+
+}
