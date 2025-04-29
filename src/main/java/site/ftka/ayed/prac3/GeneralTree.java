f7dc198fcfc0169f6d8a570dc1b6a35aa48782f1
frentuka <frentuka@gmail.com>
2025-04-28 22:07:51 -0300

    null-safe toString to print whole general trees

--- src/main/java/site/ftka/ayed/prac3/GeneralTree.java
+++ src/main/java/site/ftka/ayed/prac3/GeneralTree.java
@@ -88,4 +88,85 @@ public class GeneralTree<T> {
         return 0;
     }
 
+/*
+    Some symbols I'm going to need for symmetric design:
+
+    ╔ -> First children
+    ╠ -> Intermediate children
+    ╬ -> Middle children for unpair trees
+    ╚ -> Last children
+
+-> Symmetric design:
+    How it should look with a pair amount of children:
+    ╔ A
+    ╠ B
+X ══╣
+    ╠ C
+    ╚ D
+
+    How it should look with an unpair amount of children:
+    ╔ A
+    ╠ B
+X ══╬ C
+    ╠ D
+    ╚ E
+
+
+    Symbols for non-symmetric design:
+    ╦ -> First children (if there's more than 1)
+    ═ -> First children (if there's only 1)
+    ╠ -> Middle children
+    ╚ -> Last children
+
+-> Non-symmetric design:
+X ══╦ A
+    ╠ B
+    ╠ C
+    ╚ D
+*/
+
+
+    @Override
+    public String toString() {
+        // base: no children
+        if (isLeaf())
+            return isEmpty() ? "Null" : data.toString();
+        return toString("");
+    }
+
+    public String toString(String prefix) {
+        if (isLeaf())
+            return toString();
+
+        String data = getData() == null ? "Null" : getData().toString();
+        prefix+= " ".repeat(data.length() + 2);
+
+        // base case: only 1 children
+        if (getChildren().size() == 1)
+            return data + " ══ " + getChildren().getFirst().toString(prefix);
+
+        // exception case: multiple children (funny part)
+        StringBuilder toReturn = new StringBuilder();
+        toReturn.append(data).append(" ═");
+
+        for (int i = 0; i < getChildren().size(); i++) {
+            // is it the first?
+            if (i == 0)
+                toReturn.append("╦ ")
+                        .append(getChildren().get(i).toString(prefix + "║ "))
+                        .append("\n");
+            else
+            if (i == getChildren().size() - 1) // which is not 0
+                toReturn.append(prefix)
+                        .append("╚ ")
+                        .append(getChildren().get(i).toString(prefix + "║ "));
+            else
+                toReturn.append(prefix)
+                        .append("╠ ")
+                        .append(getChildren().get(i).toString(prefix + "║ "))
+                        .append("\n");
+        }
+
+        return toReturn.toString();
+    }
 }
